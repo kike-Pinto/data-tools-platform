52e99be5052852598e4ee8da271050ea8198131c
kike-pinto <cripintort@gmail.com>
2026-04-01 15:19:16 -0300

    Checklist de cobertura

---
+++ src/content/tool-content-status.ts
@@ -0,0 +1,30 @@
+// checklist de cobertura
+
+export const toolContentPriority = {
+  high: [
+    'csv-to-json',
+    'json-to-csv',
+    'csv-cleaner',
+    'remove-duplicates-csv',
+    'csv-to-excel',
+    'excel-to-csv',
+    'csv-validator',
+    'csv-sorter',
+  ],
+  medium: [
+    'remove-empty-columns-csv',
+    'csv-column-selector',
+    'csv-header-renamer',
+    'csv-filter-rows',
+    'csv-add-column',
+    'csv-remove-column-selectable',
+    'csv-column-merger',
+  ],
+  lower: [
+    'excel-to-json',
+    'merge-csv-files',
+    'split-csv-file',
+    'csv-column-remover',
+    'csv-row-counter',
+  ],
+}
